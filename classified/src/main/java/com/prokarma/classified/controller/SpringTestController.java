042e59546c3e5b1aee78c0f3f43488c708ece158
myeswanth <myeswanth@myeswanth>
2015-09-21 22:13:27 +0530

    initial commit

---
+++ classified/src/main/java/com/prokarma/classified/controller/SpringTestController.java
@@ -0,0 +1,16 @@
+package com.prokarma.classified.controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/service/greeting")
+public class SpringTestController {
+	@RequestMapping(value = "/{name}", method = RequestMethod.GET)
+	public String getGreeting(@PathVariable String name) {
+		String result = "Hello " + name;
+		return result;
+	}
+}
