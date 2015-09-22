ebbcf72c1a9c61fc04114be180a5ae160dcd039b
myeswanth <myeswanth@myeswanth>
2015-09-22 20:17:25 +0530

    removed files

--- classified/src/main/java/com/prokarma/classified/dao/CategoryDAO.java
+++
@@ -1,9 +0,0 @@
-package com.prokarma.classified.dao;
-
-import com.prokarma.classified.model.Category;
-
-public interface CategoryDAO extends GenericDAOHibernate<Category>{
-	
-	public Category getCategoryById(int id);
-	
-}
