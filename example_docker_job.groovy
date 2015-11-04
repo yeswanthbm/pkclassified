703d147c1ee5753151d6741a0e9fa95036440f08
yeswanthbm <yeswanth.madem@gmail.com>
2015-11-04 13:45:38 +0530

    Update example_docker_job.groovy

--- example_docker_job.groovy
+++ example_docker_job.groovy
@@ -2,7 +2,8 @@ freeStyleJob('freestyle job') {
     logRotator(-1, 10)
     scm {
         svn('https://svn2.prokarma.com/svn/dockertestapp/branches/junitmavenexample')
-	credentials('kpandurangarao')
+	userName('kpandurangarao')
+        password('admin@123')
     }
     steps {
         maven('clean install')
