6116ee9771398ead6c6b9a52396224276edb8a91
yeswanthbm <yeswanth.madem@gmail.com>
2015-11-09 15:24:26 +0530

    Update example_docker_job.groovy

--- example_docker_job.groovy
+++ example_docker_job.groovy
@@ -1,13 +1,9 @@
 freeStyleJob('freestyle job') {
-	configure { project ->
-        project / builders << 'org.foo.FooBuilder' {
-            credentialsId('foo-password')
-        }
-    }
     scm {
 	svn {
 		location('https://svn2.prokarma.com/svn/dockertestapp/branches/junitmavenexample') {
                 depth(SvnDepth.EMPTY)
+                credentialsId('foo-password')
             }
         }
 	
